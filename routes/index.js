2ddab303e5eaec6ed1ea95e5caf8d73187caff1e
Teemu Huttunen <teemu@huttunen.fi>
2016-05-16 00:48:00 +0300

    history fetch

--- routes/index.js
+++ routes/index.js
@@ -62,6 +62,62 @@ var fetchShipData = function(domain, res) {
 	});
 };
 
+var fetchShipDataHistory = function(domain, res) {
+	request(domain, function(err, resp, html) {
+		if (err) {
+			console.error(err);
+			return res.json({"status":"internal error"}, 500);
+		}
+
+		require("jsdom").env("", function (err, window) {
+			if (err) {
+				console.error(err);
+				return res.json({"status":"internal error"}, 500);
+			}
+
+			var $ = require("jquery")(window);
+
+			var shipData = [];
+
+			var tbody = $(html).find('h2').each(function (index, value) {
+				//console.log($(value).text());
+				if ($(value).text().indexOf('ja Hernesaari') >= 0) {
+					//console.log('found port!');
+					var table  = $(value).next('table').find('tbody').find('tr').each(function (index, value) {
+						var shipItem = {};
+						$(value).find('td').each(function (index, td) {
+							//console.log($(td).text());
+
+							$(td).find('span').each(function(index, value) {
+								if ($(value).attr('id').indexOf('NimiLbl') >= 0) {
+									shipItem.shipName = $(value).text();
+								} else if ($(value).attr('id').indexOf('VarustamoLbl') >= 0) {
+									shipItem.firmName = $(value).text();
+								} else if ($(value).attr('id').indexOf('SaapuiLbl') >= 0) {
+									shipItem.arrivalTime = $(value).text();
+								}
+							});
+							if (shipItem.shipName || shipItem.firmName || shipItem.arrivalTime) {
+								shipData.push(shipItem);
+							}
+						});
+					});
+				}
+			});
+
+			myCache.set("shipDataHistory", shipData, function( err, success ){
+				if( !err && success ){
+					//console.log('ship data saved to cache');
+				} else if (err) {
+					console.error(err);
+				}
+			});
+
+			return res.status(200).json(shipData);
+		});
+	});
+};
+
 router.get('/shipData', function (req, res, next) {
 	var domain = 'http://www.portofhelsinki.fi/tavaraliikenne/saapuvat_alukset';
 
@@ -69,6 +125,22 @@ router.get('/shipData', function (req, res, next) {
 		if( !err ){
 			if(value == undefined){
 				fetchShipData(domain, res);
+			}else{
+				//console.log('cache hit!');
+
+				return res.status(200).json(value);
+			}
+		}
+	});
+});
+
+router.get('/shipDataHistory', function (req, res, next) {
+	var domainHistory = 'http://www.portofhelsinki.fi/tavaraliikenne/viiden_edellisen_paivan_liikenne';
+
+	myCache.get( "shipDataHistory", function( err, value ){
+		if( !err ){
+			if(value == undefined){
+				fetchShipDataHistory(domainHistory, res);
 			}else{
 				//console.log('cache hit!');
 
