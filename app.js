f693ca451a195d4838e101fccbc264ea90839737
Teemu Huttunen <teemu@huttunen.fi>
2016-05-15 15:59:25 +0300

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'example.com');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 
