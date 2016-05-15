4d285483d75381496fcf952499e5dcbfe26140ca
Teemu Huttunen <teemu@huttunen.fi>
2016-05-15 18:40:06 +0300

    reduced logging, added node-cache with 5 min ttl

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 var request = require('request');
 
-/* GET home page. */
+var NodeCache = require( "node-cache" );
+var myCache = new NodeCache( { stdTTL: 300, checkperiod: 120 } );
+
+
 router.get('/', function (req, res, next) {
 	res.render('index');
 
 });
 
-router.get('/shipData', function (req, res, next) {
-	var domain = 'http://www.portofhelsinki.fi/tavaraliikenne/saapuvat_alukset';
+var fetchShipData = function(domain, res) {
 	request(domain, function(err, resp, html) {
 		if (err) {
 			console.error(err);
@@ -28,6 +30,7 @@ router.get('/shipData', function (req, res, next) {
 			var tbody = $(html).find('a#lsatama').parent().next('table').find('tbody');
 
 			var shipData = [];
+
 			tbody.find('tr').each(function (index, value) {
 				$(value).find('td').each(function (index, td) {
 					var shipItem = {};
@@ -46,8 +49,32 @@ router.get('/shipData', function (req, res, next) {
 				});
 			});
 
-			return res.json(shipData, 200);
+			myCache.set( "shipData", shipData, function( err, success ){
+				if( !err && success ){
+					//console.log('ship data saved to cache');
+				} else if (err) {
+					console.error(err);
+				}
+			});
+
+			return res.status(200).json(shipData);
 		});
+	});
+};
+
+router.get('/shipData', function (req, res, next) {
+	var domain = 'http://www.portofhelsinki.fi/tavaraliikenne/saapuvat_alukset';
+
+	myCache.get( "shipData", function( err, value ){
+		if( !err ){
+			if(value == undefined){
+				fetchShipData(domain, res);
+			}else{
+				//console.log('cache hit!');
+
+				return res.status(200).json(value);
+			}
+		}
 	});
 });
 
