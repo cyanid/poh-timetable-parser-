59022b82edd611dfef3ae5ac48aeac9246e40970
Teemu Huttunen <teemu@huttunen.fi>
2016-05-15 15:36:59 +0300

    allow cors

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'example.com');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
